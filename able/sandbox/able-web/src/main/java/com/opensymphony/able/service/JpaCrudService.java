ede5053beacda13a25ea1ec0cf5ac86c3f7f8521
jstrachan <jstrachan@73d2a400-3d77-8e3b-169d-3d2d98ab22f7>
2006-09-27 10:42:26 +0000

    added a test case and an example of querying using native SQL

--- able/sandbox/able-web/src/main/java/com/opensymphony/able/service/JpaCrudService.java
+++ able/sandbox/able-web/src/main/java/com/opensymphony/able/service/JpaCrudService.java
@@ -19,9 +19,13 @@ package com.opensymphony.able.service;
 
 import com.opensymphony.able.util.Log;
 import net.sourceforge.stripes.integration.spring.SpringBean;
+import org.springframework.orm.jpa.JpaCallback;
 import org.springframework.orm.jpa.JpaTemplate;
 
+import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.List;
@@ -88,7 +92,7 @@ public abstract class JpaCrudService<E> implements CrudService<E> {
     protected List find(String queryText, Object... parameters) {
         return getJpaTemplate().find(queryText, parameters);
     }
-    
+
     protected Object findFirst(String queryText) {
         return first(getJpaTemplate().find(queryText));
     }
@@ -105,6 +109,79 @@ public abstract class JpaCrudService<E> implements CrudService<E> {
             return null;
         }
         return list.get(0);
+    }
+
+/*
+    protected List findUsingSql(final String sql) {
+        return findUsingSql(sql, getEntityClass());
+    }
+    
+    protected List findUsingSql(final String sql) {
+        return getJpaTemplate().executeFind(new JpaCallback() {
+            public Object doInJpa(EntityManager entityManager) throws PersistenceException {
+                return entityManager.createNativeQuery(sql).getResultList();
+            }
+        });
+    }
+
+    protected List findUsingSql(final String sql, final Class returnType) {
+        return getJpaTemplate().executeFind(new JpaCallback() {
+            public Object doInJpa(EntityManager entityManager) throws PersistenceException {
+                return entityManager.createNativeQuery(sql, returnType).getResultList();
+            }
+        });
+    }
+
+    protected List findUsingSql(final String sql, final String resultSetMapping) {
+        return getJpaTemplate().executeFind(new JpaCallback() {
+            public Object doInJpa(EntityManager entityManager) throws PersistenceException {
+                return entityManager.createNativeQuery(sql, resultSetMapping).getResultList();
+            }
+        });
+    }
+
+*/
+
+    /**
+     * Queries the current entities using native SQL and optional parameters
+     */
+    protected List findUsingSql(final String sql, final Object... parameters) {
+        return findTypeUsingSql(sql, getEntityClass(), parameters);
+    }
+
+    /**
+     * Quries entities of the given type using native SQL and optional parameters
+     */
+    protected List findTypeUsingSql(final String sql, final Class returnType, final Object... parameters) {
+        return getJpaTemplate().executeFind(new JpaCallback() {
+            public Object doInJpa(EntityManager entityManager) throws PersistenceException {
+                System.out.println("About to perform sql: " + sql + " to return type: " + returnType);
+                Query query = entityManager.createNativeQuery(sql, returnType);
+                int index = 1;
+                for (Object parameter : parameters) {
+                    System.out.println("Setting param: " + index + " to: " + parameter);
+                    query.setParameter(index++, parameter);
+                }
+                return query.getResultList();
+            }
+        });
+    }
+
+    /**
+     * Performs a native SQL query with optional parameters using a named SQL result set mapping
+     */
+    protected List findUsingSqlResultSetMapping(final String sql, final String resultSetMapping, final Object... parameters) {
+        return getJpaTemplate().executeFind(new JpaCallback() {
+            public Object doInJpa(EntityManager entityManager) throws PersistenceException {
+                Query query = entityManager.createNativeQuery(sql, resultSetMapping);
+                int index = 1;
+                for (Object parameter : parameters) {
+                    System.out.println("Setting param: " + index + " to: " + parameter);
+                    query.setParameter(index++, parameter);
+                }
+                return query.getResultList();
+            }
+        });
     }
 
 }
