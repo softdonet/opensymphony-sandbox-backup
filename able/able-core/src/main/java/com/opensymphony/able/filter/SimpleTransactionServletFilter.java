7bcad5e776868216e8ee7655a402879c7333c69a
larham <larham@73d2a400-3d77-8e3b-169d-3d2d98ab22f7>
2007-04-05 04:26:25 +0000

    let runtime exceptions escape try/catch so that Hibernate can understand when it needs to create a table, for example.

--- able/able-core/src/main/java/com/opensymphony/able/filter/SimpleTransactionServletFilter.java
+++ able/able-core/src/main/java/com/opensymphony/able/filter/SimpleTransactionServletFilter.java
@@ -85,6 +85,9 @@ public class SimpleTransactionServletFilter implements Filter {
                         }
                         return null;
                     }
+                    catch (RuntimeException e) {
+                        throw e;
+                    }
                     catch (Exception e) {
                         return e;
                     }
